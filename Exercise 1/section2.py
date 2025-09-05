3c050d2252e26952759f55da840e0cff54f734c9
tomi832 <tomere832@gmail.com>
2025-09-05 16:32:46 +0300

    fixed in section 2 a str explicit casting that isn't relevant anymore

--- Exercise 1/section2.py
+++ Exercise 1/section2.py
@@ -2,7 +2,7 @@ def sum_digit(n: str):
     """Returns the sum of the digits of n."""
     if not str(n).isdecimal():
         return "invalid input"
-    return sum(map(int, str(n)))
+    return sum(map(int, n))
 
 def main():
     userInput = input("Please enter a number:")
