f56e44381aa881c8e9e122dbe34cf4f796f7425b
tomi832 <tomere832@gmail.com>
2025-09-05 18:01:03 +0300

    finished section 5

---
+++ Exercise 1/section5.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+def add_3_dicts(d1, d2, d3):
+    items = list(d1.items())
+    items += list(d2.items())
+    items += list(d3.items())
+    return reduce(
+        lambda acc, x: { **acc, x[0]: tuple(dict.fromkeys(acc.get(x[0], ()) + (x[1],)))},
+        items,
+        {}
+    )
+    # from what I found online, fromkeys removes duplicates while preserving order,
+    # which would be better than set which doesn't preserve order, though we weren't tasked to do so
