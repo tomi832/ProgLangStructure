88bc74f23531df9554d7dccff90529e411f93fa6
yosefk7 <yosefk7@gmail.com>
2025-09-06 22:31:38 +0300

    finished section 4 and update

--- Exercise 1/section4.py
+++ Exercise 1/section4.py
@@ -7,9 +7,13 @@ def is_prime_number(num, denom=2):
     if num/denom <= 2:
         return True
     return is_prime_number(num, denom + 1)
-print(is_prime_number(29))  # True
-print(is_prime_number(15))  # False
-print(is_prime_number(1))  # True
-print(is_prime_number(2))  # True
-print(is_prime_number(3))  # True
-print(is_prime_number(4))  # False
+
+
+if __name__ == "__main__":
+    number = int(input("Enter a number, or a negative number to exit: "))
+    while number >=0:
+        if is_prime_number(number):
+            print(f"{number} is a prime number.")
+        else:
+            print(f"{number} is not a prime number.")
+        number = int(input("Enter a number, or a negative number to exit: "))
