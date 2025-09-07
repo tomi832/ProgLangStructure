02ff9ad97e2b96a8d6e47799f5dd32593c0fa107
tomi832 <tomere832@gmail.com>
2025-09-07 18:34:39 +0300

    fixed section 2 so it works with negatives too, while not violating Functional Programming

--- Exercise 1/section2.py
+++ Exercise 1/section2.py
@@ -1,11 +1,19 @@
 def sum_digit(n: str):
     """Returns the sum of the digits of n."""
-    if not str(n).isdecimal():
+    negative = False
+    if n[0] == '-':
+        negative = True
+        if not str(n[1:]).isdecimal():
+            return "invalid input"
+    elif not str(n).isdecimal():
         return "invalid input"
-    return sum(map(int, n))
+    return -sum(map(int, n[1:])) if negative else sum(map(int, n))
+    # this went through multiple iterations, previously I accidentally violated FP
+    # by doing stuff like n = n[1:] or having 2 variables
 
 def main():
-    userInput = input("Please enter a number:")
+    print("enter number:")
+    userInput = input()
     print(sum_digit(userInput))
 
 if __name__ == "__main__":
